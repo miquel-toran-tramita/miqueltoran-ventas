1796bee1ab147dd1605578693dc70ed1071194a3
Scripted <miquel@tramita.info>
2024-09-16 20:08:31 +0200

    first stage

---
+++ src/components/HorizontalSlides.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<style lang="scss">
+  .horizontal-slides {
+    height: 70vh;
+    width: 100%;
+    display: flex;
+    overflow-x: scroll;
+    scroll-snap-type: x mandatory;
+
+    :global(.slide) {
+      min-width: 100vw;
+      scroll-snap-align: center;
+    }
+  }
+</style>
+
+<div class="horizontal-slides">
+  <slot></slot>
+</div>
