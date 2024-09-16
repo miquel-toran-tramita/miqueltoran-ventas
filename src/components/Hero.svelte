14f70bae705d47360f8c085ab55a68ded6c03bfb
miquel-toran-tramita <miquel@tramita.info>
2024-09-17 01:10:48 +0200

    asdadd

---
+++ src/components/Hero.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+</script>
+
+<style lang="scss">
+  @import 'src/sass/mixins.scss';
+
+  .hero {
+    height: 80vh;
+
+    .g-wrapper {
+      max-width: 1200px;
+    }
+
+    h1 span {
+      display: block;
+      font-size: 200px;
+      font-family: 'Oswald';
+      line-height: 1;
+      text-transform: uppercase;
+
+      @include notDesktop {
+        font-size: 60px;
+      }
+    }
+
+    .reveal {
+      color: #0000;
+      --g: no-repeat linear-gradient(var(--colorBrand) 0 0) 0 0;
+      background: var(--g), var(--g);
+      background-size: 0 100%;
+      -webkit-background-clip: padding-box, text;
+      background-clip: padding-box, text;
+      -webkit-box-decoration-break: clone;
+      box-decoration-break: clone;
+      animation:
+        t 1.2s 0.5s both,
+        b 1.2s 1.3s both;
+
+      &.delay {
+        animation-delay: 2s;
+      }
+    }
+
+    @keyframes t {
+      to {
+        background-size: 150% 100%;
+      }
+    }
+    @keyframes b {
+      to {
+        background-position:
+          -200% 0,
+          0 0;
+      }
+    }
+  }
+</style>
+
+<div class="hero">
+  <div class="g-wrapper">
+    <h1>
+      <span class="reveal">Potenciador</span>
+      <span class="reveal delay">Digital</span>
+    </h1>
+  </div>
+</div>
---
+++ src/components/Menu.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let HTMLMenu: HTMLElement
+  let active: boolean = false
+
+  onMount(() => {
+    document.addEventListener('scroll', () => {
+      const distance = HTMLMenu.getBoundingClientRect().top
+
+      active = distance <= 0
+    })
+  })
+</script>
+
+<style lang="scss">
+  @import 'src/sass/mixins.scss';
+
+  .menu {
+    position: sticky;
+    top: 0px;
+    width: 100%;
+    backdrop-filter: blur(2px);
+    margin-top: calc(30vh - 60px);
+    height: 80px;
+    z-index: 10;
+    background-color: var(--colorBackground);
+
+    .g-wrapper {
+      max-width: 1200px;
+    }
+
+    &.active {
+      transition: 0.3s ease;
+      background-color: rgba(0, 0, 0, 0.7);
+
+      .logo,
+      a {
+        opacity: 1 !important;
+      }
+    }
+
+    .g-wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 100%;
+
+      .logo {
+        transition: 0.3s ease;
+        font-size: 40px;
+        font-family: 'Oswald';
+        margin-left: 8px;
+
+        @include notDesktop {
+          font-size: 20px !important;
+        }
+      }
+
+      ul {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+        a {
+          transition: 0.3s ease;
+          font-family: 'Oswald';
+          font-size: 20px;
+          opacity: 0;
+        }
+      }
+    }
+  }
+</style>
+
+<div class="menu" class:active bind:this={HTMLMenu}>
+  <div class="g-wrapper">
+    <h2 class="logo">Miquel Torán</h2>
+
+    <ul>
+      <li>
+        <a href="">Inicio</a>
+      </li>
+      <li>
+        <a href="">Que hago</a>
+      </li>
+    </ul>
+  </div>
+</div>
