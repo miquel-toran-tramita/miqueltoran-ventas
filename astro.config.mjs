865e4183fca1ab21468ff19339758f6fbc473771
Scripted <miquel@tramita.info>
2024-09-16 12:59:32 +0200

    texto de venta

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.example.com',
+  site: 'https://www.miqueltoran.com',
   integrations: [svelte(), sitemap(), robots()],
   output: 'static', // or 'hybrid'
   server: {
